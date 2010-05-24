d6ec6bb3738f30f4daf888ecb6b6d49047ae7716
bookis <vegan.bookis@gmail.com>
2010-05-23 23:49:00 -0700

    add rdoc comments

--- lib/twitter-photos.rb
+++ lib/twitter-photos.rb
@@ -31,14 +31,17 @@ class TwitterPhoto
   end
   
   # Fetch photos from Twitpic, yFrog, Twitgoo, and TweetPhoto.
+  # 
   # @example Find photo by bookis only from yfrog and twitpic
   #   TwitterPhoto.get_photos_by('bookis', :tweetphoto => false, :yfrog => true, :twitpic => true, :twitgoo => false)
+  # 
   # @option options [true, false] :tweetphoto default true
   # @option options [true, false] :twitpic default true
   # @option options [true, false] :yfrog default true
   # @option options [true, false] :twitgoo default true
+  # 
   # @return [TwitterPhoto] A collection of TwitterPhoto objects will be returned
-  @param :username
+  # @param :username
   def self.get_photos_by(username, options={})
     @pix = []
     hydra = Typhoeus::Hydra.new
